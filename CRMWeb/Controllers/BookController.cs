e438d3c513392198e7e5cf6680fb31dd01969243
Asad Khan <asadkhan033@gmail.com>
2021-10-16 18:23:46 +0530

    Add project files.

---
+++ CRMWeb/Controllers/BookController.cs
@@ -0,0 +1,36 @@
+﻿using CRMWeb.Models;
+using CRMWeb.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRMWeb.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository = null;
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+        }
+
+        public ViewResult getAllBooks()
+        {
+            var view =  _bookRepository.getAllBooks();
+            return View(view);
+        }
+
+        public BookModel getBook(int id)
+        {
+            return _bookRepository.getBookById(id);
+        }
+
+        public List<BookModel> searchBooks(string title, string author)
+        {
+            return _bookRepository.searchBooks(title, author);
+        }
+
+    }
+}
---
+++ CRMWeb/Repository/BookRepository.cs
@@ -0,0 +1,41 @@
+﻿using CRMWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRMWeb.Repository
+{
+    public class BookRepository
+    {
+
+        public List<Models.BookModel> getAllBooks()
+        {
+            return DataSource();
+
+
+        }
+
+        public BookModel getBookById(int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public List<BookModel> searchBooks(string title, string author)
+        {
+            return DataSource().Where(x => x.Title.Contains(title) || x.Author.Contains(author)).ToList();
+        }
+
+        public List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel{Id = 1, Title = "MVC" , Author = "Asad" },
+                new BookModel{Id = 2, Title = "Java" , Author = "Asad" },
+                new BookModel{Id = 3, Title = "C#" , Author = "Khan" },
+                new BookModel{Id = 4, Title = "ASP.NET" , Author = "Asd" },
+                new BookModel{Id = 5, Title = "DSA" , Author = "Baba" }
+            };
+        }
+    }
+}
